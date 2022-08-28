a2278891f4b41ff422c53f149a066790c673a06a
HRe7ma <58150907+HRe7ma@users.noreply.github.com>
2022-08-28 12:43:09 +0200

    Add files via upload

---
+++ boom.js
@@ -0,0 +1,129 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+
+
+	
+  function myFunctionn() {
+  
+  
+  $('.my').show("slow");
+  $('.mys').show("slow");
+  $('.myg').show("slow");
+  $('.myh').show("slow");
+  $('.myk').show("slow");
+  $('.myb').show("slow");
+  
+  $('.mm').show("slow");
+  $('.nn').show("slow");
+  $('.ppp').show("slow");
+  
+  };
+  
+  
+    
+  function fub() {
+  
+  $('.mm').show("slow");
+  $('.nn').show("slow");
+  $('.ppp').show("slow");
+  $('.my').hide("slow");
+  $('.mys').hide("slow");
+  $('.myg').hide("slow");
+  $('.myh').hide("slow");
+  $('.myk').hide("slow");
+  $('.myb').hide("slow");
+  
+  
+  
+  };
+  
+  
+  
+  
+  function myFunctio() {
+  
+  $('.my').show("slow");
+  $('.mys').show("slow");
+  $('.myg').show("slow");
+  $('.mm').hide("slow");
+  $('.nn').hide("slow");
+  $('.ppp').hide("slow");
+  $('.myh').hide("slow");
+  $('.myk').hide("slow");
+  $('.myb').hide("slow");
+  
+  };
+  
+  
+  
+    
+  function myFuncti() {
+  
+  $('.myh').show("slow");
+  $('.myk').show("slow");
+  $('.myb').show("slow");
+  $('.my').hide("slow");
+  $('.mys').hide("slow");
+  $('.myg').hide("slow");
+  $('.mm').hide("slow");
+  $('.nn').hide("slow");
+  $('.ppp').hide("slow");
+  
+
+  
+  };
+  
+  
+
+  
+  
+    function myl() {
+    
+  
+  
+  document.getElementById('mlk').style = 'color: #d63031;';
+  document.getElementById('bnt').style = 'color: #636e72;';
+  document.getElementById('lok').style = 'color: #636e72;';
+  document.getElementById('lki').style = 'color: #636e72;';
+  
+  };
+
+  
+  
+    function myul() {
+    
+  
+  
+  
+  document.getElementById('bnt').style = 'color: #d63031;';
+  document.getElementById('mlk').style = 'color: #636e72;';
+  document.getElementById('lok').style = 'color: #636e72;';
+  document.getElementById('lki').style = 'color: #636e72;';
+  
+  };
+  
+  
+
+  
+    function mqyl() {
+    
+  
+  document.getElementById('lok').style = 'color: #d63031;';
+  document.getElementById('mlk').style = 'color: #636e72;';
+  document.getElementById('bnt').style = 'color: #636e72;';
+  document.getElementById('lki').style = 'color: #636e72;';
+  
+  };
+
+  
+  
+    function mvyl() {
+    
+  
+  document.getElementById('lki').style = 'color: #d63031;';
+  document.getElementById('mlk').style = 'color: #636e72;';
+  document.getElementById('bnt').style = 'color: #636e72;';
+  document.getElementById('lok').style = 'color: #636e72;';
+  
+  };
+  
+  
